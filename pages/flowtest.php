c8f71c77fb58fedf54a9d5493d823c7e7fd9e5b7
Tim Lytle <tim@timlytle.net>
2011-08-08 01:17:41 -0400

    Added Say Test. Allow testing without hanging up the browser phone.

--- pages/flowtest.php
+++ pages/flowtest.php
@@ -1,45 +1,54 @@
 <?php 
-$ci = &get_instance();
-$ci->load->helper('form');
+$pluginData = OpenVBX::$currentPlugin->getInfo();
+require_once $pluginData['plugin_path'] . '/FlowTest.php';
+$flowTest = new flowTest(OpenVBX::$currentPlugin);
 
-//get flow array for select
-$flows = array();
-foreach(OpenVBX::getFlows() as $flow){
-    $flows[$flow->values['id']] = $flow->values['name'];
+$exception = false;
+
+if(isset($_POST['callsid'])){
+    $flowTest->setCallSid($_POST['callsid']);
 }
 
-//make an incomming request to the client, and redirect the call to the flow
-if(isset($_POST['flow']) AND array_key_exists($_POST['flow'], $flows)){
-    //use the url helper to get the site url
-    $this->load->helper('url');
-    $response = new Response();
-    //generate url to the selected flow
-    $response->addRedirect(site_url('twiml/applet/voice/' . $_POST['flow'] .'/start'));
-    //TODO: would be nice to do this without using the twimlet
-    $url = "http://twimlets.com/echo?Twiml=" . $response->asURL(true);
+try{
+    //make an incomming request to the client, and redirect the call to the flow
+    if(isset($_POST['testflow']) AND array_key_exists($_POST['flow'], $flowTest->getFlows())){
+        $flowTest->callFlow($_POST['flow']);
+    }
+    
+    //make an incomming request to the client, with a simple say script
+    if(isset($_POST['testsay'])){
+        $flowTest->callSay($_POST['say']);
+    }
+} catch (Exception $exception) {
+    
+}
 
-    //start a call to the client
-    $client = new TwilioRestClient($this->twilio_sid,$this->twilio_token);
-    $response = $client->request(
-    	"Accounts/".$this->twilio_sid."/Calls",
-        "POST",
-         array("Caller" => 'client:' . OpenVBX::getCurrentUser()->id,
-               "To" => 	'client:' . OpenVBX::getCurrentUser()->id,
-               "Url" => $url
-         )
-    );
-} 
 ?>
 
 <div class="vbx-plugin">
-<?php if(isset($response) AND $response->IsError):?>
+<?php if($exception):?>
 	<div class="notify">
 		<p class="message">Could not call your OpenVBX Browser Phone - is it online?<a href class="close action"></a></p>
 	</div>
 <?php endif;?>
-<p>Select a flow to test using the OpenVBX Browser Phone:</p>
-<form action="" method="post">
-    <?php echo form_dropdown('flow', $flows) ?>
-    <button class="submit-button ui-state-focus" type="submit"><span>Test Flow</span></button>
-</form>
+<?php if($flowTest->getCallSid()):?>
+	<div class="notify">
+		<p class="message">Connected to the OpenVBX Browser Phone. You can continue to test without hanging up.<a href class="close action"></a></p>
+	</div>
+<?php endif;?>
+    <h2>Flow Test</h2>
+    <p></p>
+    <p>Select a flow to test using the OpenVBX Browser Phone:</p>
+    <form action="" method="post">
+        <?php echo form_dropdown('flow', $flowTest->getFlows()) ?>
+        <button class="submit-button ui-state-focus" type="submit" name="testflow"><span>Test Flow</span></button>
+        <input type="hidden" name="callsid" value="<?php echo $flowTest->getCallSid()?>">
+
+    <h2>Say Test</h2>
+    <p>Test the text-to-speech engine with this text:</p>
+    
+        <textarea name="say"><?php echo htmlentities($_POST['say'])?></textarea>
+        <button class="submit-button ui-state-focus" type="submit" name="testsay"><span>Read Text</span></button>
+        <input type="hidden" name="callsid" value="<?php echo $flowTest->getCallSid()?>">
+    </form>
 </div>
